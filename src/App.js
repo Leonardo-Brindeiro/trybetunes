bf31cbbb69168b9f2ce501f8392361adcb096f6f
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2022-08-30 18:27:26 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
-
+import { Route, Switch } from 'react-router';
+import Login from './pages/Login';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import NotFound from './pages/NotFound';
+// mentoria andré
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <p>TrybeTunes</p>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route path="" component={ NotFound } />
+        </Switch>
+      </div>
+    );
   }
 }
 
